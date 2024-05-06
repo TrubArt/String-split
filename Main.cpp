ed0569390a09575435f3c0fbb4053ee758d31b13
TrubArt <vkag03@mail.ru>
2024-05-06 11:30:27 +0300

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+std::vector<std::string> getSequence(const std::string& str, const std::string& delimiter)
+{
+	std::vector<std::string> answ;
+	size_t posNew = std::string::npos;
+	int posLast = -1 * delimiter.size();
+	std::string token;
+
+	while ((posNew = str.find(delimiter, posLast + delimiter.size())) != std::string::npos) {
+		token = str.substr(posLast + delimiter.size(), posNew - posLast - delimiter.size());
+		if (!token.empty())
+		{
+			answ.push_back(token);
+		}
+		posLast = posNew;
+		token.erase();
+	}
+
+	token = str.substr(posLast + delimiter.size());
+	if (!token.empty())
+	{
+		answ.push_back(token);
+	}
+
+	return answ;
+}
